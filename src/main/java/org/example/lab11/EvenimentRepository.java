fb32608daa5ebf4f5a20c5372ac05281f69cc614
groza890 <catalingroza89@gmail.com>
2025-01-13 00:28:33 +0200

    temalab11

--- src/main/java/org/example/lab11/EvenimentRepository.java
+++ src/main/java/org/example/lab11/EvenimentRepository.java
@@ -1,4 +1,12 @@
 package org.example.lab11;
 
-public class EvenimentRepository {
+import org.example.lab11.Eveniment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface EvenimentRepository extends JpaRepository<Eveniment,Integer>{
+    List<Eveniment> findByLocatia(String locatia);
+    List<Eveniment> findByData(LocalDate data);
 }
